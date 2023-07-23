0e036f4e1bebbc3615ba8e9f5584232d10c231f2
ZHANG Cheng <czhang.oss@gmail.com>
2023-07-24 00:36:40 +0800

    Updated to use latest stable (v9.2.20)

--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 [![Build Status](https://travis-ci.org/johnbywater/es-example-shareholding.svg?branch=master)](https://travis-ci.org/johnbywater/es-example-shareholding)
 [![Coverage Status](https://coveralls.io/repos/github/johnbywater/es-example-shareholding/badge.svg?branch=master)](https://coveralls.io/github/johnbywater/es-example-shareholding)
 
-Example "shareholding" application using the Python eventsourcing library
+Example "shareholding" application using the Python eventsourcing library.
+
+To use SQLAlchemy [persistence](https://eventsourcing.readthedocs.io/en/v9.2.20/topics/persistence.html), do something like:
+```Shell
+export PERSISTENCE_MODULE='eventsourcing_sqlalchemy'
+export SQLALCHEMY_URL='sqlite:///:memory:'
+```
--- test.py
+++ test.py
@@ -2,67 +2,65 @@ from datetime import datetime
 from decimal import Decimal
 from unittest import TestCase
 
-from eventsourcing.application.sqlalchemy import SQLAlchemyApplication
-
 from shareholding.application import ShareholdingApplication
 from shareholding.domainmodel import ShareClass, Shareholding
 
 
 class TestShareholdingApplication(TestCase):
     def test_shareholding(self):
-        with ShareholdingApplication.mixin(SQLAlchemyApplication)() as app:
-            # A situation...
-            person = app.register_person(name="Joe")
-            self.assertEqual(person.name, "Joe")
-            company = app.register_company(name="Acme")
+        app = ShareholdingApplication()
+        # A situation...
+        person = app.register_person(name="Joe")
+        self.assertEqual(person.name, "Joe")
+        company = app.register_company(name="Acme")
 
-            # Something happens...
-            shareholding = company.allot_shares(
-                person_id=person.id,
-                number_of_shares=10,
-                share_class_name="ordinary",
-                nominal_value_per_share=Decimal("1.00"),
-                price_paid_per_share=Decimal("1.00"),
-                currency="GBP",
-                allotted_on=datetime.now(),
-            )
+        # Something happens...
+        shareholding = company.allot_shares(
+            person_id=person.id,
+            number_of_shares=10,
+            share_class_name="ordinary",
+            nominal_value_per_share=Decimal("1.00"),
+            price_paid_per_share=Decimal("1.00"),
+            currency="GBP",
+            allotted_on=datetime.now(),
+        )
 
-            self.assertIsInstance(shareholding, Shareholding)
-            self.assertEqual(shareholding.shareholder, person.id)
-            self.assertEqual(shareholding.number_of_shares, 10)
-            self.assertEqual(shareholding.share_class_name, "ordinary")
+        self.assertIsInstance(shareholding, Shareholding)
+        self.assertEqual(shareholding.shareholder, person.id)
+        self.assertEqual(shareholding.number_of_shares, 10)
+        self.assertEqual(shareholding.share_class_name, "ordinary")
 
-            share_class_name = shareholding.share_class_name
-            share_class = company.get_shareclass(share_class_name)
-            self.assertIsInstance(share_class, ShareClass)
-            self.assertEqual(share_class.name, "ordinary")
-            self.assertEqual(share_class.nominal_value, Decimal("1.00"))
-            self.assertEqual(share_class.currency, "GBP")
+        share_class_name = shareholding.share_class_name
+        share_class = company.get_shareclass(share_class_name)
+        self.assertIsInstance(share_class, ShareClass)
+        self.assertEqual(share_class.name, "ordinary")
+        self.assertEqual(share_class.nominal_value, Decimal("1.00"))
+        self.assertEqual(share_class.currency, "GBP")
 
-            self.assertEqual(
-                company.count_shareholdings(person.id, share_class_name), 1
-            )
+        self.assertEqual(
+            company.count_shareholdings(person.id, share_class_name), 1
+        )
 
-            shareholding = company.allot_shares(
-                person_id=person.id,
-                number_of_shares=10,
-                share_class_name="ordinary",
-                nominal_value_per_share=Decimal("1.00"),
-                price_paid_per_share=Decimal("1.00"),
-                currency="GBP",
-                allotted_on=datetime.now(),
-            )
+        shareholding = company.allot_shares(
+            person_id=person.id,
+            number_of_shares=10,
+            share_class_name="ordinary",
+            nominal_value_per_share=Decimal("1.00"),
+            price_paid_per_share=Decimal("1.00"),
+            currency="GBP",
+            allotted_on=datetime.now(),
+        )
 
-            self.assertEqual(
-                company.count_shareholdings(person.id, share_class_name), 2
-            )
+        self.assertEqual(
+            company.count_shareholdings(person.id, share_class_name), 2
+        )
 
-            # company.issue_shares(
-            #     person_id=person.id,
-            #     number_of_shares=10,
-            #     share_class_name="ordinary",
-            #     nominal_value_per_share=Decimal("1.00"),
-            #     price_paid_per_share=Decimal("1.00"),
-            #     currency='GBP',
-            #     issued_on=datetime.now(),
-            # )
+        # company.issue_shares(
+        #     person_id=person.id,
+        #     number_of_shares=10,
+        #     share_class_name="ordinary",
+        #     nominal_value_per_share=Decimal("1.00"),
+        #     price_paid_per_share=Decimal("1.00"),
+        #     currency='GBP',
+        #     issued_on=datetime.now(),
+        # )
